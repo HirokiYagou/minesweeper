24f349c39dac521a9f4d0bf1eee5a239dfb666a8
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-03 01:14:40 +0900

    test.js削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../game")
-require("../test")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
